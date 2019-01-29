109b65d961035cf68ceee666cc42d0f14c80a889
mxgorbig <aniddem@hotmail.com>
2019-01-28 20:50:33 -0600

    Fin de la seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -5,18 +5,29 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 @Injectable({
   providedIn: 'root'
 })
-export class InfoPaginaService {
-  iinfo: InfoPagina = {};
-  cargada = false;
+  export class InfoPaginaService {
+    iinfo: InfoPagina = {};
+    cargada = false;
+    equipo: any[] = [];
 
-  constructor( private http: HttpClient) {
+    constructor( private http: HttpClient) {
+      this.cargarInfo();
+      this.cargarEquipo();
+    }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
-      .subscribe( (resp: InfoPagina) => {
+      .subscribe((resp: InfoPagina) => {
         this.cargada = true;
         this.iinfo = resp;
-        console.log(resp);
+      });
+  }
 
-    });
-
-   }
+  private cargarEquipo() {
+    this.http
+      .get("https://angula-html-9836a.firebaseio.com/equipo.json")
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+      });
+  }
 }
